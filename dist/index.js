03fab5b6ad58adaf6a2209675164e04e4ab5944d
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-07-09 23:03:37 +0530

    .

--- dist/index.js
+++ dist/index.js
@@ -7,20 +7,18 @@ const express_1 = __importDefault(require("express"));
 const server_config_1 = require("./config/server.config");
 // import authRouter from "./routes/auth.routes";
 // import messageRouter from "./routes/message.routes";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.text());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: true,
-//     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.text());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({
+    origin: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+}));
 app.get("/", (req, res) => {
     return res.status(200).json({
         status: 200,
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ import express from "express";
 import { PORT } from "./config/server.config";
 // import authRouter from "./routes/auth.routes";
 // import messageRouter from "./routes/message.routes";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.text());
-// app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(cookieParser());
 
-// app.use(
-//   cors({
-//     origin: true,
-//     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   return res.status(200).json({
