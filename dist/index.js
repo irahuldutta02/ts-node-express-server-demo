c6fbb433c95d98d659ce6233f26445cc24bc581e
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-07-09 22:58:36 +0530

    .

--- dist/index.js
+++ dist/index.js
@@ -7,13 +7,12 @@ const express_1 = __importDefault(require("express"));
 const server_config_1 = require("./config/server.config");
 const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const message_routes_1 = __importDefault(require("./routes/message.routes"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use(express_1.default.urlencoded({ extended: true }));
-app.use(express_1.default.text());
-app.use((0, cookie_parser_1.default)());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.text());
+// app.use(cookieParser());
 app.use((0, cors_1.default)({
     origin: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.text());
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.text());
+// app.use(cookieParser());
 
 app.use(
   cors({
